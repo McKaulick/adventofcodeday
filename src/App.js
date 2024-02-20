8e009d8aa1b7c26d572b6df4dcfefe41fba75118
Patrice Fiset <patrice.fiset@gmail.com>
2024-02-20 11:48:07 -0500

    Day 1 completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
 import logo from './pluginIcon.png';
 import './App.css';
-import React, { Component } from 'react';
+import React from 'react';
 import { useState, useEffect } from 'react';
 
+function getTotal(stringValues) {
+  var total = "";
+  var totalString = "";
+  var totalString1 = "";
+  var count = 0;
+  var totalString2 = "";
+
+  for (let i = 0; i < stringValues.length; i++) {
+    for (const char of stringValues[i]) {
+      if (!isNaN(char)) {
+        count = count + 1;
+        if (totalString1 == "") {
+          totalString1 = totalString1 + char;
+        }
+        else {
+          totalString2 = "";
+          totalString2 = char;
+        }
+      }
+    }
+    if (count == 1) {
+      totalString = totalString1 + totalString1;
+    }
+    else {
+      totalString = totalString1 + totalString2;
+    }
+    total = Number(total) + Number(totalString);
+    count = 0;
+    totalString = "";
+    totalString1 = "";
+    totalString2 = "";
+  }
+  return "total: "+total; //55,538
+}
 
 function App() {
 
+  //Declare useState
   const [txtData, setTxtData] = useState("En cours de chargement...");
 
-
+  // Declare useEffect that fetch the data in the text file
   useEffect(() => {
     fetch(process.env.PUBLIC_URL + "/input.txt")
     .then(r => r.text())
@@ -17,10 +52,7 @@ function App() {
     });
   }, [])
 
-  var s = txtData;
-  s.split(" ").forEach(character => console.log(character));
-
-  const typo2 = typeof(txtData);
+  var txtArray = txtData.split("\n");
 
    return (
     <div className="App">
@@ -39,10 +71,13 @@ function App() {
         </a>
       </header>
       <div className="App-body">
-        {txtData}
+        <div className="display-linebreak">
+           {getTotal(txtArray)} 
+        </div>
       </div>
     </div>
   );
 }
 
 export default App;
+
